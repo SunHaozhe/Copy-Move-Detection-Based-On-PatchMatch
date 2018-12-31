ab9dcff001749f0d5ab546a29730867edc458380
Sun-Haozhe <275940200@QQ.COM>
2018-12-31 16:47:23 +0100

    filtering on NNF and Distance Field

--- src/main.py
+++ src/main.py
@@ -24,14 +24,18 @@ modified_path = os.path.join(modified_dir, name + "_." + jpg )
 
 img = Image.open(modified_path)
 I = np.asarray(img)
-mpm = M_PatchMatch(I, patch_size=16, get_dist=True)
+mpm = M_PatchMatch(I, patch_size=10, get_dist=True)
 
 #mpm = M_PatchMatch(np.random.uniform(size=(768, 1024, 3)))
 #mpm = M_PatchMatch(np.ones((7, 9)), patch_size=1, border_size=1)
 nnf, df = mpm.run(nb_iter=5)
 
+# post-processing
+thr = 20
+nnf, df = filtering(thr, nnf, df)
+
 
 print(nnf.shape)
 print("Done in %.4f s." % (time() - t0))
 
-visualize_nnf(nnf, df)
+visualize(nnf, df, img, filename)
--- src/utils.py
+++ src/utils.py
@@ -1,34 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import os
 
-def visualize_nnf(nnf, df):
-	plt.figure(figsize=(12, 6.5))
+def visualize(nnf, df, img, input_filename):
+	fig = plt.figure(figsize=(12, 6.5))
 	plt.suptitle("Nearest Neighbor Field map (absolute value)")
 
-	ax = plt.subplot(221)
+	ax = plt.subplot(231)
 	x = np.linspace(0, nnf[:, :, 0].shape[1], num=nnf[:, :, 0].shape[1]) 
 	y = np.linspace(0, nnf[:, :, 0].shape[0], num=nnf[:, :, 0].shape[0])
 	X, Y = np.meshgrid(x , y) 
 	plt.pcolormesh(X, Y, np.abs(nnf[:, :, 0]), cmap = cm.copper) 
 	plt.colorbar()
+	plt.gca().invert_yaxis()
 	ax.set_title("First dimension (axis)")
 
-	ax = plt.subplot(222)
+	ax = plt.subplot(232)
 	plt.pcolormesh(X, Y, np.abs(nnf[:, :, 1]), cmap = cm.copper) 
 	plt.colorbar()
+	plt.gca().invert_yaxis()
 	ax.set_title("Second dimension (axis)")
 
-	ax = plt.subplot(223)
+	ax = plt.subplot(233)
 	plt.pcolormesh(X, Y, 
 				   np.abs(nnf[:, :, 0]) + np.abs(nnf[:, :, 1]), 
 				   cmap = cm.copper) 
 	plt.colorbar()
+	plt.gca().invert_yaxis()
 	ax.set_title("L1 norm of NNF map")
 
-	ax = plt.subplot(224)
-	plt.pcolormesh(X, Y, df[:, :], cmap = cm.copper) 
+	ax = plt.subplot(234)
+	plt.pcolormesh(X, Y, df, cmap = cm.copper) 
 	plt.colorbar()
+	plt.gca().invert_yaxis()
 	ax.set_title("Distance field map")
 
+	ax = plt.subplot(235)
+	plt.pcolormesh(X, Y, np.log(df), cmap = cm.copper) 
+	plt.colorbar()
+	plt.gca().invert_yaxis()
+	ax.set_title("Distance field map in log scale")
+
+	ax = plt.subplot(236)
+	plt.imshow(img)
+	ax.set_title("Image")
+
+	dir_path = "../output"
+	output_path = os.path.join(dir_path, 
+							   input_filename.split(".")[0] + "_output.jpg")
+	if not os.path.exists(dir_path):
+		os.makedirs(dir_path)
+
+	fig.savefig(output_path, dpi=fig.dpi)
 	plt.show()
+
+
+def filtering(thr, nnf, df):
+	h = nnf.shape[0]
+	w = nnf.shape[1]
+	max_value = np.max(df)
+	quantile_value = np.quantile(df, 0.5)
+
+	for i in range(h):
+		for j in range(w):
+			if np.sum(np.abs(nnf[i, j, :])) < thr:
+				nnf[i, j, :] = np.array([0, 0])
+				df[i, j] = max_value
+
+
+	for i in range(h):
+		for j in range(w):
+			if df[i, j] > quantile_value:
+				df[i, j] = max_value
+
+	return nnf, df
